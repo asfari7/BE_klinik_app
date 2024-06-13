39f9ef4756800e5704aa29df8515831bc004d854
asfari7 <muhammadasnafi4@gmail.com>
2024-06-13 15:54:55 +0700

    fix cors origin

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const app = express();
 
 const corsOptions = {
     origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization,Origin,X-Requested-With,Accept,Access-Control-Allow-Origin',
 }
 
-app.use(cors);
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
